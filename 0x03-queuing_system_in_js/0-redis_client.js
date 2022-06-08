b869da41273026fc8de821eafa86cc7e92453bf8
Nehemiah Kamolu <kamolunehemiah@gmail.com>
2022-06-08 08:19:55 +0300

    redis: set, get, hset, hgetall

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -1,7 +1,7 @@
-import redis from 'redis';
+import { createClient } from 'redis';
 
-const redis_client = redis.createClient();
+const client = createClient();
 
-redis_client.on('connect', () => console.log('Redis client connected to the server'));
+client.on('connect', () => console.log('Redis client connected to the server'));
 
-redis_client.on('error', (error) => console.log(`Redis client not connected to the server: ${error.message}`));
+client.on('error', (error) => console.log(`Redis client not connected to the server: ${error.message}`));
